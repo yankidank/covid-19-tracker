d86a38d793716a092358f1f81205d1f637101f64
Ashely <arumbaoa10@gmail.com>
2020-04-08 20:04:18 -0700

    table & dummy data in seed

--- schema.sql
+++ schema.sql
@@ -1,2 +1,15 @@
 CREATE DATABASE covid19;
 USE covid19;
+
+CREATE TABLE stats
+(
+	id int NOT NULL AUTO_INCREMENT,
+    city VARCHAR(100) NULL,
+    province VARCHAR(500) NULL,
+    country VARCHAR(100) NULL,
+    confirmed INTEGER default 0,
+    deaths INTEGER default 0,
+    recovery INTEGER default 0,
+    last_update datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key(id)
+    );
