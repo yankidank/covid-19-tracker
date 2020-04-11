cbf66b8640b778738fd9d4d6a31838ea5181aa09
Robert Filkin <rfilkin17@gmail.com>
2020-04-11 11:54:07 -0700

    removed sub-repo

--- csv_to_json_test/read-file.js
+++ csv_to_json_test/read-file.js
@@ -1,5 +1,6 @@
 const csvjson = require('csvjson');
 const readFile = require('fs').readFile;
+const fs = require('fs');
 
 var mysql = require("mysql");
 
@@ -19,60 +20,81 @@ var connection = mysql.createConnection({
     database: "covid19"
 });
 
+//data from https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports
+
 connection.connect(function(err) {
     if (err) throw err;
+    
+    fs.readdir("./data_folder/daily_reports/", function (err, files) {
 
-    readFile('./test-data.csv', 'utf-8', (err, fileContent) => {
-        if(err) {
-            console.log(err); // Do something to handle the error or just throw it
-            throw new Error(err);
-        }
+        files.forEach(function (file, index) {
+            //grabs the name of each file in the directory, saves in variable "file"
+            console.log(file);
 
-        const jsonObj = csvjson.toObject(fileContent);
-        //console.log(jsonObj);
+            if (file != "README.md"){
 
-        for (var i = 0; i < jsonObj.length; i++){
-            var day = jsonObj[i];
-            console.log(day);
+                readFile('./data_folder/daily_reports/' + file, 'utf-8', (err, fileContent) => {
+                    if(err) {
+                        console.log(err); // Do something to handle the error or just throw it
+                        throw new Error(err);
+                    }
 
-            //filtering in case of empty fields
-            var Confirmed;
-            if (day.Confirmed == ""){
-                Confirmed = 0;
-            }
-            else{
-                Confirmed = parseInt(day.Confirmed)
-            }
-            var Deaths;
-            if (day.Deaths == ""){
-                Deaths = 0;
-            }
-            else{
-                Deaths = parseInt(day.Deaths)
-            }
-            var Recovered;
-            if (day.Recovered == ""){
-                Recovered = 0;
-            }
-            else{
-                Recovered = parseInt(day.Recovered)
-            }
+                    const jsonObj = csvjson.toObject(fileContent);
+                    //console.log(jsonObj);
 
-            //date handling
-            
-            var day_moment = moment(day['Last Update'], "M-DD-YYYY hh:mm");
-            console.log(day_moment.format("YYYY-MM-DD hh:mm"));
-            var day_datetime = day_moment.format("YYYY-MM-DD hh:mm")
+                    for (var i = 0; i < jsonObj.length; i++){
+                        var day = jsonObj[i];
+                        //console.log(day);
 
-            var query = `INSERT INTO stats (province, country, confirmed, deaths, recovery, last_update) 
-            VALUES ("` + day['Province/State'] + `", "` + day['Country/Region'] +
-            `", ` + Confirmed + `, ` + Deaths + `, ` + Recovered + `, "` + day_datetime + `");`;
-            connection.query(query, function(err, res) {
-                if (err) throw err;
-                console.log("\n");
-                console.table(res);
-            });
-        }
-        connection.end();
+                        //filtering in case of empty fields
+                        var Province;
+                        if (day['Province/State'] == ""){
+                            console.log("blank province");
+                            Province = "";
+                        }
+                        else{
+                            Province = day['Province/State'];
+                        }
+                        var Confirmed;
+                        if (day.Confirmed == ""){
+                            Confirmed = 0;
+                        }
+                        else{
+                            Confirmed = parseInt(day.Confirmed)
+                        }
+                        var Deaths;
+                        if (day.Deaths == ""){
+                            Deaths = 0;
+                        }
+                        else{
+                            Deaths = parseInt(day.Deaths)
+                        }
+                        var Recovered;
+                        if (day.Recovered == ""){
+                            Recovered = 0;
+                        }
+                        else{
+                            Recovered = parseInt(day.Recovered)
+                        }
+
+                        //date handling
+                        
+                        var day_moment = moment(day['Last Update'], "M/DD/YYYY HH:mm");
+                        console.log(day_moment.format("YYYY-MM-DD HH:mm"));
+                        var day_datetime = day_moment.format("YYYY-MM-DD HH:mm");
+
+                        var query = `INSERT INTO stats (province, country, confirmed, deaths, recovery, last_update) 
+                        VALUES ("` + Province + `", "` + day['Country/Region'] +
+                        `", ` + Confirmed + `, ` + Deaths + `, ` + Recovered + `, "` + day_datetime + `");`;
+                        connection.query(query, function(err, res) {
+                            if (err) throw err;
+                            console.log("\n");
+                            console.table(res);
+                        });
+                    }
+                    
+                });
+            }
+        });
     });
 });
