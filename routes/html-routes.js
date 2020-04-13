ddd2286f420fb74964f282e7c1b99fc30622a77b
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-04-13 15:46:50 -0700

    Signup and Login buttons in navbar are now functional. Removed button when logged in and displayed a welcome message. Removed older dedicated login  pages.

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -17,26 +17,4 @@ module.exports = function (app) {
     res.render("index", testObj);
     // res.sendFile(path.join(__dirname, "../public/members.html"));
   });
-
-  app.get("/login", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/login.html"));
-  });
-
-  app.get("/signup", function (req, res) {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
-    res.sendFile(path.join(__dirname, "../public/signup.html"));
-  });
-
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get("/members", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
-  });
 };
