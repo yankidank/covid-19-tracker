b05fec262c2620c5d987548b4297f8a2994c719d
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-04-14 13:23:45 -0700

    Added numbers for US statistics using an ajax call

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,4 +1,25 @@
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 $(document).ready(function() {
+
+  $.ajax('https://covidtracking.com/api/us', 
+  {
+    dataType: 'json', // type of response data
+    timeout: 500,     // timeout milliseconds
+    success: function (data) {   // success callback function
+      $('#countConfirmed').text(numberWithCommas(data[0].positive));
+      $('#countRecovered').text(numberWithCommas(data[0].recovered));
+      $('#countDeaths').text(numberWithCommas(data[0].death));
+      console.log(data)
+      console.log('counter updated')
+    },
+    error: function (errorMessage) { // error callback 
+      console.log('Error: ' + errorMessage)
+    }
+  });
+
   // GET request to figure out which user is logged in
   // updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
@@ -76,3 +97,27 @@ $('#navbar-toggle').click(function() {
   $('#navbarBasicExample, .navbar-menu').toggleClass('is-active');
   $('.navbar-burger').toggleClass('is-active');
 });
+
+/* 
+[
+  {
+  "positive": 579390,
+  "negative": 2393818,
+  "pending": 16574,
+  "hospitalizedCurrently": 53086,
+  "hospitalizedCumulative": 63459,
+  "inIcuCurrently": 13915,
+  "inIcuCumulative": 1646,
+  "onVentilatorCurrently": 6148,
+  "onVentilatorCumulative": 221,
+  "recovered": 36299,
+  "hash": "b8c86a54731066776520600de5f5ec4d5bd929a4",
+  "lastModified": "2020-04-14T19:12:15.053Z",
+  "death": 23529,
+  "hospitalized": 63459,
+  "total": 2989782,
+  "totalTestResults": 2973208,
+  "posNeg": 2973208,
+  "notes": "NOTE: \"total\", \"posNeg\", \"hospitalized\" will be removed in the future."
+  }
+] */
