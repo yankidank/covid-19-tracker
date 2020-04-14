e71e8d9a7ed382503a50b7f0b54e197ffa1856b0
Eric Heikkinen <18619+yankidank@users.noreply.github.com>
2020-04-14 10:30:43 -0700

    Partial work to change newsletter subscription when logged in. Needs additional work in dashboard.js and api-routes.js

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,10 +2,52 @@ $(document).ready(function() {
   // GET request to figure out which user is logged in
   // updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
-    if (data.email){
+    if (data.email){      
       $('.buttons').css("display", "none");
       $(".member-name").text('Welcome Back '+data.email);
-      //console.log('Subscribed: '+data.subscription)
+      console.log(data.subscription)
+      if (data.subscription){
+        // Check it
+        $(".member-name").append(`<div class="b-checkbox is-success is-circular">
+        <input name="subscription-navbar" id="subscription-navbar" class="styled" checked type="checkbox">
+        <label for="subscription-navbar" id="subscription-message">
+            Subscribed to Newsletter
+        </label>
+    </div>`)
+      } else {
+        $(".member-name").append(`<div class="b-checkbox is-success is-circular">
+        <input name="subscription-navbar" id="subscription-navbar" class="styled" type="checkbox">
+        <label for="subscription-navbar" id="subscription-message">
+            Subscribe to Newsletter
+        </label>
+    </div>`)
+      }
+      $("#subscription-navbar").click(function(){
+        if (data.subscription){
+          $.ajax({
+            url: '/api/subscribe/'+data.id,
+            type: 'PUT',
+            data: {subscription:false},
+            success: function(data) {
+              console.log('Unsubscribed')
+              console.log(data)
+            }
+          });
+        } else {
+          $.ajax({
+            url: '/api/subscribe/'+data.id,
+            type: 'PUT',
+            data: {subscription:true},
+            success: function(data) {
+              console.log('Subscribed')
+              console.log(data)
+            }
+          });
+        }
+        
+        $('#subscription-message').text($('#subscription-message').text() == 'Subscribe to Newsletter' ? 'Subscribed to Newsletter' : 'Subscribe to Newsletter');
+      }); 
+      
     } else {
       $('.buttons').css("display", "block");
     }
