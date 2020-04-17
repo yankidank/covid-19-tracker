4de87703bc592a60f04e751d916eebba6cec44c9
Abelard Bautista <abelardbautista@gmail.com>
2020-04-16 19:25:05 -0700

    Revert "Started working on pinging current search location."
    This reverts commit 77292aeda5ed574a609e721111cbcad1c9db5bd5.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,55 +2,49 @@ function numberWithCommas(x) {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
-$(document).ready(function () {
+$(document).ready(function() {
+
   //global stats
-  function getGlobalStats() {
+  function getGlobalStats(){
     var queryURL = "https://api.covid19api.com/summary";
     $.ajax({
       url: queryURL,
-      method: "GET",
-    })
-      .then(function (response) {
-        $("#countConfirmed").text(
-          numberWithCommas(response.Global.TotalConfirmed)
-        );
-        $("#countRecovered").text(
-          numberWithCommas(response.Global.TotalRecovered)
-        );
-        $("#countDeaths").text(numberWithCommas(response.Global.TotalDeaths));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+      method: "GET"
+    }).then(function(response) {
+      $('#countTitle').text('Global Statistics');
+      $('#countConfirmed').text(numberWithCommas(response.Global.TotalConfirmed));
+      $('#countRecovered').text(numberWithCommas(response.Global.TotalRecovered));
+      $('#countDeaths').text(numberWithCommas(response.Global.TotalDeaths));
+    }).catch(function(error){
+      console.log(error)
+    });
   }
-  getGlobalStats();
+  getGlobalStats()
 
-  // Heatmap Data
-  var addressPoints = [];
-  async function getMapData() {
+  // Heatmap Data 
+  var addressPoints = []
+  async function getMapData(){
     $.ajax({
-      url: "/api/covid_data/deaths",
-      method: "GET",
-    })
-      .then(function (response) {
-        for (let index = 0; index < response.length; index++) {
-          const element = response[index];
-          const arrayConstruct = [];
-          arrayConstruct.push(element.latitude);
-          arrayConstruct.push(element.longitude);
-          arrayConstruct.push(element.deaths);
-          addressPoints.push(arrayConstruct);
-        }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+      url: '/api/covid_data/deaths',
+      method: "GET"
+    }).then(function(response) {
+      for (let index = 0; index < response.length; index++) {
+        const element = response[index];
+        const arrayConstruct = []
+        arrayConstruct.push(element.latitude)
+        arrayConstruct.push(element.longitude)
+        arrayConstruct.push(element.deaths)
+        addressPoints.push(arrayConstruct)
+      }
+    }).catch(function(error){
+      console.log(error)
+    });
   }
 
   // Prepare the map
-  var map;
-  function generateMap(lat, lon) {
-    if (map) {
+  var map
+  function generateMap(lat,lon){
+    if (map){
       map.remove();
     }
     if (!lat || !lon) {
@@ -60,34 +54,32 @@ $(document).ready(function () {
       // Zoom map to latitude and longitude
       map = L.map("map").setView([lat, lon], 6);
     }
-    // Set theme, try 'DarkGray'
+      // Set theme, try 'DarkGray'
     L.esri.basemapLayer("Gray").addTo(map);
-    L.heatLayer(addressPoints).addTo(map), (draw = true);
+    L.heatLayer(addressPoints).addTo(map), draw = true;
   }
 
   // IP to location API
-  async function ipSearch() {
+  async function ipSearch(){
     $.ajax({
-      url: "http://ip-api.com/json?callback=?",
+      url: 'http://ip-api.com/json?callback=?',
       method: "GET",
-      dataType: "json",
-      jsonp: "callback",
-    })
-      .then(function (response) {
-        //console.log(response)
-        $("#country_input").val(response.country);
-        console.log("performSearch " + response.country);
-        performSearch(response.country);
-        generateMap(response.lat, response.lon);
-        console.log("Location from IP API");
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+      dataType: 'json',       
+      jsonp: 'callback'
+    }).then(function(response) {
+      //console.log(response)
+      $("#country_input").val(response.country)
+      console.log("performSearch "+response.country)
+      performSearch(response.country);
+      generateMap(response.lat, response.lon)
+      console.log("Location from IP API");
+    }).catch(function(error){
+      console.log(error)
+    });
   }
 
   // Request User's Location
-  /*   async function getLocation() {
+/*   async function getLocation() {
     navigator.geolocation.getCurrentPosition(function(position) {
       console.log('Location from Browser')
       navigator.geolocation.getCurrentPosition(showPosition);
@@ -100,106 +92,101 @@ $(document).ready(function () {
       }
     });
   } */
-
-  function showPosition(position) {
-    generateMap(position.coords.latitude, position.coords.longitude);
+  
+  function showPosition(position) { 
+    generateMap(position.coords.latitude, position.coords.longitude)
   }
 
   // Add heatmap data
   addressPoints = addressPoints.map(function (p) {
-    return [p[0], p[1]];
+      return [p[0], p[1]];
   });
 
   //generateMap()
   getMapData().then(generateMap);
 
-  $("#icon-geolocate").click(function () {
-    $("#icon-geolocate").toggleClass("fa-crosshairs");
-    $("#icon-geolocate").toggleClass("fa-spinner fa-pulse");
+  $("#icon-geolocate").click(function(){
+    $("#icon-geolocate").toggleClass("fa-crosshairs")
+    $("#icon-geolocate").toggleClass("fa-spinner fa-pulse")
     ipSearch();
-    $("#icon-geolocate").toggleClass("fa-crosshairs");
-    $("#icon-geolocate").toggleClass("fa-spinner fa-pulse");
-  });
-
+    $("#icon-geolocate").toggleClass("fa-crosshairs")
+    $("#icon-geolocate").toggleClass("fa-spinner fa-pulse")
+  })
+  
   // GET request to figure out which user is logged in
   // updates the HTML on the page
-  $.get("/api/user_data").then(function (data) {
-    if (data.email) {
-      $(".buttons").css("display", "none");
-      $(".member-name").text("Welcome Back " + data.email);
+  $.get("/api/user_data").then(function(data) {
+    if (data.email){      
+      $('.buttons').css("display", "none");
+      $(".member-name").text('Welcome Back '+data.email);
       //console.log(data.subscription)
-      if (data.subscription) {
+      if (data.subscription){
         // Check it
-        $(".member-name")
-          .append(`<div class="b-checkbox is-success is-circular">
+        $(".member-name").append(`<div class="b-checkbox is-success is-circular">
         <input name="subscription-navbar" id="subscription-navbar" class="styled" checked type="checkbox">
         <label for="subscription-navbar" id="subscription-message">
             Subscribed to Newsletter
         </label>
-    </div>`);
+    </div>`)
       } else {
-        $(".member-name")
-          .append(`<div class="b-checkbox is-success is-circular">
+        $(".member-name").append(`<div class="b-checkbox is-success is-circular">
         <input name="subscription-navbar" id="subscription-navbar" class="styled" type="checkbox">
         <label for="subscription-navbar" id="subscription-message">
             Subscribe to Newsletter
         </label>
-    </div>`);
+    </div>`)
       }
-      $("#subscription-navbar").click(function () {
-        if (data.subscription) {
+      $("#subscription-navbar").click(function(){
+        if (data.subscription){
           $.ajax({
-            url: "/api/subscribe/" + data.id,
-            type: "PUT",
-            data: { subscription: false },
-            success: function (data) {
-              console.log("Unsubscribed");
-              console.log(data);
-            },
+            url: '/api/subscribe/'+data.id,
+            type: 'PUT',
+            data: {subscription:false},
+            success: function(data) {
+              console.log('Unsubscribed')
+              console.log(data)
+            }
           });
         } else {
           $.ajax({
-            url: "/api/subscribe/" + data.id,
-            type: "PUT",
-            data: { subscription: true },
-            success: function (data) {
-              console.log("Subscribed");
-              console.log(data);
-            },
+            url: '/api/subscribe/'+data.id,
+            type: 'PUT',
+            data: {subscription:true},
+            success: function(data) {
+              console.log('Subscribed')
+              console.log(data)
+            }
           });
         }
-
-        $("#subscription-message").text(
-          $("#subscription-message").text() == "Subscribe to Newsletter"
-            ? "Subscribed to Newsletter"
-            : "Subscribe to Newsletter"
-        );
-      });
+        
+        $('#subscription-message').text($('#subscription-message').text() == 'Subscribe to Newsletter' ? 'Subscribed to Newsletter' : 'Subscribe to Newsletter');
+      }); 
+      
     } else {
-      $(".buttons").css("display", "block");
+      $('.buttons').css("display", "block");
     }
   });
 });
 // User Login/Signup Modal
-$("#auth-signup").click(function () {
+$("#auth-signup").click(function() {
   $("#modal-register").addClass("is-active");
 });
-$("#auth-login").click(function () {
+$("#auth-login").click(function() {
   $("#modal-signup").addClass("is-active");
 });
-$(".exit-modal").click(function () {
+$(".exit-modal").click(function() {
   $("#modal-signup").removeClass("is-active");
-  $("#modal-register").removeClass("is-active");
+  $( "#modal-register").removeClass("is-active");
 });
 // Escape key press exits modal
-$(document).keyup(function (e) {
+$(document).keyup(function(e) {
   if (e.key === "Escape") {
     $("#modal-signup").removeClass("is-active");
     $("#modal-register").removeClass("is-active");
   }
 });
 // Navbar menu toggle
-$("#navbar-toggle").click(function () {
-  $("#navbarBasicExample, .navbar-menu").toggleClass("is-active");
-  $(".navbar-burger").toggleClass("is-active");
+$('#navbar-toggle').click(function() {
+  $('#navbarBasicExample, .navbar-menu').toggleClass('is-active');
+  $('.navbar-burger').toggleClass('is-active');
 });
